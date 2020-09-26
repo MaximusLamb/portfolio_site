c9efd01276192ef77c0448fa0a4e877e324d7f37
Max Lamb <maxlambcodes@gmail.com>
2020-09-26 09:18:56 -0700

    some stuff done, dont want to burn myself out trying to get back into coding

---
+++ portfolio_site/src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+      <img src="placekitten.com/200/200" alt="kitten"></img>
+      <p>Frick</p>
+      </div>
+      )
+    }
+  }
